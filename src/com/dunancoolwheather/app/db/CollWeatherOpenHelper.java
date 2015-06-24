f8f6bfb3a74401326a565df26767463ace417c7b
dylan4723 <1686426598@qq.com>
2015-06-24 14:18:05 +0800

    First commit

---
+++ src/com/dunancoolwheather/app/db/CollWeatherOpenHelper.java
@@ -0,0 +1,45 @@
+package com.dunancoolwheather.app.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class CollWeatherOpenHelper extends SQLiteOpenHelper {
+
+	private static final String CREATE_PROVINCE = "create table Province ("
+			+ "id integer primary key autoincrement, " 
+			+ "province_name text, "
+			+ "province_code text)";
+	private static final String CREATE_CITY = "create table City ("
+			+ "id integer primary key autoincrement, " 
+			+ "city_name text, " 
+			+ "city_code text, " 
+			+ "province_id integer)";
+	private static final String CREATE_COUNTY = "create table County ("
+			+ "id integer primary key autoincrement, " 
+			+ "county_name text, " 
+			+ "county_code text, " 
+			+ "city_id integer)";
+
+	public CollWeatherOpenHelper(Context context, String name,
+			CursorFactory factory, int version) {
+		super(context, name, factory, version);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// TODO Auto-generated method stub
+		db.execSQL(CREATE_PROVINCE); // 创建Province表
+		db.execSQL(CREATE_CITY); // 创建City表
+		db.execSQL(CREATE_COUNTY); // 创建County表
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
